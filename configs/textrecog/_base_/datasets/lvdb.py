2c504ce27665f2b0bed5b29d6247e3ac9e8e422c
Mekacher-Anis <anismkdev@gmail.com>
2022-10-11 16:50:58 +0200

    [MIG] Converted LVDB recog to new format.

---
+++ configs/textrecog/_base_/datasets/lvdb.py
@@ -0,0 +1,23 @@
+dataset_type = 'OCRDataset'
+data_root = 'data/lv'
+
+lvdb_rec_train = dict(
+    type=dataset_type,
+    data_root=data_root,
+    ann_file='recog_train_label.json',
+    test_mode=False,
+    pipeline=None)
+
+lvdb_rec_val = dict(
+    type=dataset_type,
+    data_root=data_root,
+    ann_file='recog_val_label.json',
+    test_mode=False,
+    pipeline=None)
+
+lvdb_rec_test = dict(
+    type=dataset_type,
+    data_root=data_root,
+    ann_file='recog_val_label_subset.json',
+    test_mode=False,
+    pipeline=None)
